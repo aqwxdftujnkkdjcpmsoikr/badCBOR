306ef01d570ad7ad65773d15f9e454555e4c58a5
OJ <oscar.jozon@etu.emse.fr>
2022-02-08 10:30:09 +0100

    Improvements on clarity, flag support and usage. Readme updated.

--- parse.c
+++ parse.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+// for the getopt
+
+
 
 struct charStack {
     char * string;
@@ -40,6 +44,36 @@ struct str * consume(struct charStack **s) {
     return ret;
 }
 
+
+
+struct intStack {
+    int val;
+    void * next;
+};
+void stackInt(struct intStack **s, int newVal) {
+    struct intStack * newHead = malloc(sizeof(struct intStack));
+    newHead->val=newVal;
+    newHead->next=&(**s);
+    *s = newHead;
+    return;
+}
+void deStackInt(struct intStack **s) {
+    struct intStack * stemp = (*s)->next;
+    free((*s));
+    *s = stemp;
+    return;
+}
+int eatInt(struct intStack **s) {
+    if ((*s)->val==0) {
+      deStackInt(s);
+      return eatInt(s);
+    }
+    else {
+      --((*s)->val);
+      return (*s)->val;
+    }
+    return 0;
+}
 
 
 int ipow(int base, int exp)
@@ -110,13 +144,19 @@ unsigned char intLen(unsigned char len, unsigned char * stream, int index) {
     return 0;
 }
 
-char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
-    printf("[start of conversion]\n");
+char * binToCBOR_S2S(unsigned char * inStream, int streamLen, char debugLevel) {
+    char * SEQUENCE_SEPARATOR = ", ";
+    int SEQUENCE_SEPARATOR_LEN = 2;
+    if(debugLevel>1) printf("[start of conversion]\n");
     char * ret;
     // first iteration to evaluate the needed output length
     int estLen=0;
     //unsigned char intLen[5] = {0,3,5,10,20};
-    printf("  [estimating length of final string]\n");
+    if(debugLevel>1) printf("  [estimating length of final string]\n");
+    struct intStack * lenOfCurrentScopeStack= malloc(sizeof(struct intStack));
+    lenOfCurrentScopeStack->val = -1;
+    lenOfCurrentScopeStack->next = lenOfCurrentScopeStack; // never will be used
+
     for(int byteInd=0; byteInd<streamLen; ++byteInd) {
         // integers : we can read int size and convert :
             // uint64 > 20 chars
@@ -160,19 +200,26 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
                 realLen = (realLen<<8)+inStream[byteInd];
             }
         }
+
+        eatInt(&lenOfCurrentScopeStack);
+
         if(type == 0) {estLen += realLen;}
         else if(type == 1) {estLen += realLen+1;}
         else if(type == 2) {estLen += realLen*8+(realLen-1)+4;}
         else if(type == 3) {estLen += realLen+2;}
-        else if(type == 4) {estLen += (realLen-1)*2+2;}
-        else if(type == 5) {estLen += realLen*3+(realLen-1)*2+2;}
+        else if(type == 4) {estLen += (realLen-1)*2+2;stackInt(&lenOfCurrentScopeStack, realLen);}
+        else if(type == 5) {estLen += realLen*3+(realLen-1)*2+2;stackInt(&lenOfCurrentScopeStack, realLen*2);}
         else if(type == 6) {estLen += 0;}
         else if(type == 7) {estLen += 0;}
         if(type==2 || type==3) {
             // skip data bytes
             byteInd+=realLen;
         }
-        printf("    (type %d , len %d) -> %d\n", type, realLen, estLen);
+        if(debugLevel>2) printf("    (type %d , len %d) -> %d\n", type, realLen, estLen);
+    }
+    if ((lenOfCurrentScopeStack->val) < -1) {
+        if(debugLevel>1) printf("  adjusting for shitty formatting (sequence) : +%d\n", (-(lenOfCurrentScopeStack->val)-2)*SEQUENCE_SEPARATOR_LEN);
+        estLen += SEQUENCE_SEPARATOR_LEN*(-(lenOfCurrentScopeStack->val)-2);
     }
     // With this, estLen should be roughtly the final string length (or above !)
     ret = malloc((estLen+1)*sizeof(char)); // so we're sure to have enough room for a final null byte
@@ -181,13 +228,13 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
     for(int foo=0;foo<estLen;++foo) {
         ret[foo]=32;
     }
-    printf("  [estimated length : %d]\n", estLen);
+    if(debugLevel>1) printf("  [estimated length : %d]\n", estLen);
     // WRITING CURSOR
     int cursor = 0;
     // INIT CHAR STACK
     struct charStack *myCharStack = malloc(sizeof(struct charStack));
-    myCharStack->string = " ";
-    myCharStack->stringLen = 1;
+    myCharStack->string = SEQUENCE_SEPARATOR;
+    myCharStack->stringLen = SEQUENCE_SEPARATOR_LEN;
     myCharStack->isEnd = 0;
     myCharStack->next = myCharStack;
     // INIT SCOPE MAP LEN STACK
@@ -195,7 +242,7 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
     // myMapLenStack->val=0;
     // myMapLenStack->next=myMapLenStack;
 
-    printf("  [writing cbor string]\n");
+    if(debugLevel>1) printf("  [writing cbor string]\n");
     for(int byteInd=0; byteInd<streamLen; ++byteInd) {
         unsigned char type = ((inStream[byteInd]&224)>>5);
         unsigned char len = (inStream[byteInd]&31);
@@ -220,7 +267,7 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             }
         }
 
-        printf("    reading byte %d, type %d (%d)\n", byteInd, type, len);
+        if(debugLevel>2) printf("  reading byte %d, type %d (%d)\n", byteInd, type, len);
 
         if(type==0 || type==1) {
             if(type==1) {
@@ -228,7 +275,7 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
                 ++cursor;
             }
             if(len<24){
-                printf("    writing short int at %d\n", cursor);
+                if(debugLevel>2) printf("    writing short int at %d\n", cursor);
                 for(char i=0; i<realLen;++i) {
                     ret[cursor]=48+(len%(ipow(10,(realLen-i)))/ipow(10,(realLen-i-1)));
                     ++cursor;
@@ -237,13 +284,13 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             else {
                 // read with len-23 level of int
                 // declare max
-                printf("    writing long int at %d\n", cursor);
+                if(debugLevel>2) printf("    writing long int at %d\n", cursor);
                 unsigned long int longInt=0;
                 for(char i=0;i<(1<<(len-24));++i) {
                     ++byteInd;
                     longInt = (longInt<<8) + (unsigned long int) inStream[byteInd];
                 }
-                printf("      int is %lu (%d bytes long)\n", longInt, (1<<(len-24)));
+                if(debugLevel>2) printf("      int is %lu (%d bytes long)\n", longInt, (1<<(len-24)));
                 for(char i=0; i<realLen;++i) {
                     // printf("      writing digit '%c'\n", ((char) 48+(longInt%(ipow(10,(realLen-i)))/ipow(10,(realLen-i-1))) ) );
                     ret[cursor]=48+(longInt%(ipow(10,(realLen-i)))/ipow(10,(realLen-i-1)));
@@ -252,7 +299,7 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             }
         }
         else if(type == 2) {
-            printf("    writing byte string of length %d bytes\n", realLen);
+            if(debugLevel>2) printf("    writing byte string of length %d bytes\n", realLen);
             ret[cursor]='\'';++cursor;
             ret[cursor]='0';++cursor;
             ret[cursor]='b';++cursor;
@@ -270,8 +317,8 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             ret[cursor]='\'';++cursor;
         }
         else if(type == 3) {
-            printf("    writing string of length %d\n", realLen);
-            printf("      inserting start delimiter '\"'\n");
+            if(debugLevel>2) printf("    writing string of length %d\n", realLen);
+            if(debugLevel>2) printf("      inserting start delimiter '\"'\n");
             ret[cursor]='"';++cursor;
             add(&myCharStack,"\"",1,1);
             for(int foo=0;foo<realLen;++foo) {
@@ -280,12 +327,12 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             ++byteInd;
             int ind;
             for(ind=byteInd;ind<byteInd+realLen;++ind) {
-                printf("      printing char %c\n", inStream[ind]);
+                if(debugLevel>2) printf("      printing char %c\n", inStream[ind]);
                 ret[cursor]=inStream[ind];
                 ++cursor;
                 struct str *insert = consume(&myCharStack);
                 for(int foo=0;foo<(insert->length);++foo) {
-                    printf("      inserting spacer/end '%c' (len:%d)\n", insert->content[foo], insert->length);
+                    if(debugLevel>2) printf("      inserting spacer/end '%c' (len:%d)\n", insert->content[foo], insert->length);
                     ret[cursor]=(insert->content)[foo];
                     ++cursor;
                 }
@@ -293,8 +340,8 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             byteInd=ind-1;
         }
         else if(type == 4) {
-            printf("    writing array of length %d\n", realLen);
-            printf("      inserting start delimiter\n");
+            if(debugLevel>2) printf("    writing array of length %d\n", realLen);
+            if(debugLevel>2) printf("      inserting start delimiter\n");
             add(&myCharStack,"]",1,1);
             for(int foo=1;foo<realLen;++foo) {
               add(&myCharStack,", ",2,0);
@@ -303,8 +350,8 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             continue;
         }
         else if(type == 5) {
-            printf("    writing map of length %d\n", realLen);
-            printf("      inserting start delimiter\n");
+            if(debugLevel>2) printf("    writing map of length %d\n", realLen);
+            if(debugLevel>2) printf("      inserting start delimiter\n");
             ret[cursor]='{';++cursor;
             add(&myCharStack,"}",1,1);
             add(&myCharStack," : ",3,0);
@@ -321,24 +368,24 @@ char * binToCBOR_S2S(unsigned char * inStream, int streamLen) {
             // do nothing
         }
         struct str *insert = consume(&myCharStack);
-        for(int foo=0;foo<(insert->length);++foo) {
-            printf("  inserting spacer/end '%c' (len:%d)\n", insert->content[foo], insert->length);
+        for(int foo=0;foo<(insert->length) && cursor<estLen;++foo) {
+            if(debugLevel>2) printf("  inserting spacer/end '%c' (len:%d)\n", insert->content[foo], insert->length);
             ret[cursor]=(insert->content)[foo];
             ++cursor;
         }
         char MAX_REC=7;char rec=0;
         while (insert->isEnd && cursor<estLen && rec<MAX_REC) {
             ++rec;
-            printf("  collapsing endings (normal)\n");
+            if(debugLevel>2) printf("  collapsing endings (normal)\n");
             insert = consume(&myCharStack);
-            printf("    inserting spacer/end '%s' (len:%d)\n", insert->content, insert->length);
+            if(debugLevel>2) printf("    inserting spacer/end '%s' (len:%d)\n", insert->content, insert->length);
             for(int foo=0;foo<(insert->length) && cursor<estLen;++foo) {
                 ret[cursor]=(insert->content)[foo];
                 ++cursor;
             }
         }
     }
-    printf("[end of conversion]\n");
+    if(debugLevel>1) printf("[end of conversion]\n");
     return ret;
 }
 
@@ -369,25 +416,70 @@ void foo() {
 
 
 
+void usage(char* call) {
+  printf("\
+  Usage : %s [flags] [Input Hex String]\\n\
+  Flags :\n\
+    -v adds one level of verbose/debug (0 to 3 supported)\n\
+    -d adds one level of verbose/debug (0 to 3 supported)\n\
+    -q prints the resulting CBOR string raw\n\
+    -h prints this help\n", call);
+  return;
+}
+
+
 
 int main(int argc, char * argv[]) {
+    // get options
+    int c;
+    char verbose=0;
+    char quiet=0;
+    if (argc==1) {usage(argv[0]);exit(1);}
+
+  	while ((c = getopt(argc, argv, "dqvh")) != -1) {
+    		switch( c ) {
+    		case 'd':
+            verbose++;
+    		    break;
+    		case 'q':
+    			  quiet = 1;
+            break;
+    		case 'v':
+    		    verbose++;
+            break;
+        case 'h':
+            usage(argv[0]);exit(0);
+        default:
+            usage(argv[0]);exit(0);
+    		}
+  	}
+    argc -= optind;
+    argv += optind;
+    if (verbose>0) printf("verbose level : %d \n", verbose);
     int len=0;
-    while(argv[1][++len]);
+    while(argv[0][++len]);
     // printf("len : %d\n", len);
-    unsigned char * myDataStream = inToBin(argv[1], len);
+    unsigned char * myDataStream = inToBin(argv[0], len);
     // DEBUG PRINT
-    printf("datastream : [");
-    for(int a=0; a<len/2;++a) {
-        printf("%s", bin(myDataStream[a]));
-        if(a<(len/2-1)) printf(" ");
+    if (verbose>0) {
+        printf("datastream : [");
+        for(int a=0; a<len/2;++a) {
+            printf("%s", bin(myDataStream[a]));
+            if(a<(len/2-1)) printf(" ");
+        }
+        printf("]\n");
     }
-    printf("]\n");
     // END OF DEBUG PRINT
 
+    // Test :
     //foo();
 
-    char * cborString = binToCBOR_S2S(myDataStream, len/2);
-    printf("CBOR : <%s>\n", cborString);
+    char * cborString = binToCBOR_S2S(myDataStream, len/2, verbose);
+    if (!quiet) printf("CBOR : <%s>\n", cborString);
+    else printf("%s\n", cborString);
+    len=0;
+    while(cborString[++len]);
+    if (verbose>0) printf("Effective length : %d\n",len);
     free(cborString);
     free(myDataStream);
     return 0;
--- readme.md
+++ readme.md
@@ -20,3 +20,21 @@ Out should be :
 ```
 CBOR : <{"key" : 500, 2 : {3 : [4, 5, 6]}, "foo" : "bar", [7] : '0b01000001'}>
 ```
+
+
+Example input 2 (sequence of objects):
+```
+./parse A4636B65791901F402A1038304050663666F6F63626172810741011010
+```
+
+Out should be :
+```
+CBOR : <{"key" : 500, 2 : {3 : [4, 5, 6]}, "foo" : "bar", [7] : '0b01000001'}, 16, 16>
+```
+
+## Flags
+
+Flags "-v" "-d" and "-q" are supported.
+  + "-v" adds one level of verbose/debug (0 to 3 supported)
+  + "-d" adds one level of verbose/debug (0 to 3 supported)
+  + "-q" prints the resulting CBOR string raw
